b7616090c5a9ac9a13468bde2d652b281600768f
Zichen Liu <zl3114@ic.ac.uk>
2015-12-12 08:59:54 +0000

    added initial messages and interface for rendering

---
+++ Raytracer.cu
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+#include "kernel.h"
+
+using namespace std;
+
+#define LINE_SEPARATE "----------------------------------------------------"
+
+void handleCudaError(cudaError_t e){
+		if(e==cudaErrorInvalidDevice){
+			cout<<"UNEXPECTED DEVICE ERROR"<<endl;
+		}else if(e==cudaSuccess){
+		
+		}else{
+			cout<<"UNKNOWN ERROR"<<endl;
+		}
+}
+
+int main(){
+	int CUDA_DEVICE_NUMBER;
+	cudaDeviceProp CUDA_DEVICE_PROP;
+	int USE_GPU, SCREEN_WIDTH, SCREEN_HEIGHT;
+	string FILENAME;
+
+
+	cudaGetDeviceCount(&CUDA_DEVICE_NUMBER);
+
+	cout<<LINE_SEPARATE<<endl;
+	cout<<"WELCOME TO RAYTE v0.88 CPU/GPU RAYTRACER"<<endl;
+	cout<<"RAYTE IS OPEN SOURCE AND DISTRIBUTED UNDER"<<endl;
+	cout<<"CC4.0-ATTRIBUTION-SHAREALIKE-INTERNATIONAL"<<endl;
+	cout<<"THE SOURCE LIVES AT crynetlogistics.github.io/raytrace"<<endl;
+	cout<<"RAYTE IS AN ORIGINAL PROJECT BY ZICHEN LIU"<<endl;
+	cout<<LINE_SEPARATE<<endl;
+	cout<<"SYSTEM SPECIFICATIONS AS DETECTED;"<<endl;
+	cout<<"NUMBER OF CUDA ENABLED DEVICES DETECTED -> "<<CUDA_DEVICE_NUMBER<<endl;
+	for(int i=0; i<CUDA_DEVICE_NUMBER; i++){
+		cudaError_t e = cudaGetDeviceProperties(&CUDA_DEVICE_PROP, i);
+		handleCudaError(e);
+		cout<<"DEVICE "<<i<<" -> "<<CUDA_DEVICE_PROP.name<<endl;
+		cout<<"\tCOMPUTE VERSION -> "<<CUDA_DEVICE_PROP.major<<"."<<CUDA_DEVICE_PROP.minor<<endl;
+		cout<<"\tTOTAL GLOBAL MEMORY -> "<<CUDA_DEVICE_PROP.totalGlobalMem/(1024*1024)<<"MiB"<<endl;
+		cout<<"\tCLOCK FREQUENCY -> "<<CUDA_DEVICE_PROP.clockRate/1024<<"MHz"<<endl;
+		cout<<"\tSTREAMING MULTIPROCESSOR COUNT -> "<<CUDA_DEVICE_PROP.multiProcessorCount<<endl;
+	}
+	cout<<LINE_SEPARATE<<endl;
+
+	cout<<"PLEASE ENTER GPU_USAGE_LEVEL, SCREEN_WIDTH, SCREEN_HEIGHT, FILENAME:"<<endl;
+
+	cin>>USE_GPU>>SCREEN_WIDTH>>SCREEN_HEIGHT>>FILENAME;
+
+	raytrace(USE_GPU, SCREEN_WIDTH, SCREEN_HEIGHT, FILENAME);
+}
